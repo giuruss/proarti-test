96bfc48b96a4219a8d4cf5bc46cb4d88001c0e6e
lubuntu <giuruss@gmail.com>
2021-04-16 18:27:57 +0200

    update

--- src/DTO/DTOCreateUser.php
+++ src/DTO/DTOCreateUser.php
@@ -7,12 +7,12 @@ use Symfony\Component\Validator\Constraints as Assert;
 final class DTOCreateUser
 {
 
-    /**
-     * @Assert\Type("integer")
-     * @Assert\NotNull()
-     * @EntityExist(class=User::class)
-     */
-    public $userId;
+//    /**
+//     * @Assert\Type("integer")
+//     * @Assert\NotNull()
+//     * @EntityExist(class=User::class)
+//     */
+//    public $userId;
 
     /**
      * @Assert\Type("string")
